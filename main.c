23e959b1aa3ea7c5109392f3fcd4f9959f7547e8
Samuel Snape <c3281090@uon.edu.au>
2019-04-04 03:46:49 +0000

    Added test code form video

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    abcd
-    abcd
-    abcd
-    abcd
+    //abcd
+    //abcd
+    //abcd
+    //abcd
+    
+    //test code from videos
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("Found integers %d %d %d \n", a, b, c);
+  
   return 0:
 }
