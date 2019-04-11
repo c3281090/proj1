106aa499e07cf5f8dd60c3e0d9200f91a87a13ce
Samuel Snape <c3281090@uon.edu.au>
2019-04-11 04:38:24 +0000

    outline for project

--- main.c
+++ main.c
@@ -1,29 +1,55 @@
 #include <stdio.h>
 
-
-
-
 int main() {
+    
     //variables
-    int op; //Option chosen by user
-    
-    
+    int option; //Option chosen by user
+
     //Ask for option from list
     printf("Please choose an option between 1 [ONE] and 6 [SIX] \n");
     
-    
-    scanf("%d", &op);
+    //Scan for option chosen
+    scanf("%d", &option);
     
     //Call required  function using switch/case 
+    switch(option){
+        case 1:
+            //call function 1
+            printf("Option 1 chosen \n");
+            break;
+            
+        case 2:
+            //call function 2
+            printf("Option 2 chosen \n");
+            break;
+            
+        case 3:
+            //call function 3
+            printf("Option 3 chosen \n");
+            break;
+            
+        case 4:
+            //call function 4
+            printf("Option 4 chosen \n");
+            break;
+            
+         case 5:
+            //call function 5
+            printf("Option 5 chosen \n");
+            break;
+            
+        case 6:
+            //call function 6
+            printf("Option 6 chosen \n");
+            break;
+            
+    }
     
-    
-    
+    return(0);
     
 }
 
-
-
-
+//Functions
 
 //1. Get message, and rotation key, encrypt
 
