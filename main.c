ca7d1936b16228c43e2710c29d2c5346f8c6b72b
Samuel Snape <c3281090@uon.edu.au>
2019-04-11 04:09:21 +0000

    Outline of program added

--- main.c
+++ main.c
@@ -12,3 +12,36 @@ int main() {
     printf("Found integers %d %d %d \n", a, b, c);
   
 }
+
+
+//Choose option from list
+
+
+
+//Call required  function 
+
+
+
+//1. Get message, and rotation key, encrypt
+
+
+
+//2. Get cipher text and rotation key, decrypt
+
+
+
+//3. Get message and subsitution, encrypt
+
+
+
+//4. Get cipher text and subsitution, decrypt
+
+
+
+//5. Get cipher text, use rotation to decrypt
+
+
+
+//6. Get cipher text, use substituion to decrypt
+
+
